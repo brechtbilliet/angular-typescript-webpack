684ea88c13b4e000a5b4f8d165c2d879fc710b45
Brecht Billiet <brecht.billiet@persgroep.be>
2015-09-08 10:03:05 +0200

    optimization

--- webpack.make.js
+++ webpack.make.js
@@ -3,28 +3,19 @@
 var webpack = require('webpack');
 var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = function makeWebpackConfig(options) {
-    var browserSync = new BrowserSyncPlugin({
-        host: 'localhost',
-        port: 8080,
-        server: {
-            baseDir: 'dev'
-        },
-        ui: false,
-        online: false,
-        notify: false
-    });
-
+    console.log(options);
     var config = {
         entry: ['./app/index.ts'],
         output: {
             filename: 'build.js',
-            path: options.BUILD?'dist': 'dev'
+            path: options.BUILD ? 'dist' : 'dev'
         },
         resolve: {
             root: __dirname,
-            extensions: ['','.ts','.js','.json'],
+            extensions: ['', '.ts', '.js', '.json'],
             alias: {
                 'app': 'app'
             }
@@ -33,16 +24,6 @@ module.exports = function makeWebpackConfig(options) {
             modulesDirectories: ["node_modules"]
         },
         module: {
-            //postLoaders: [
-            //    {
-            //        test: /\.ts$/,
-            //        exclude: [
-            //            /node_modules/,
-            //            /spec.ts$/
-            //        ],
-            //        loader: 'istanbul-instrumenter'
-            //    }
-            //],
             loaders: [
 
                 {
@@ -50,14 +31,14 @@ module.exports = function makeWebpackConfig(options) {
                     loader: 'ts',
                     exclude: /node_modules/
                 },
-                 {
+                {
                     test: /\.css$/,
                     loader: 'style-loader!css-loader'
                 },
                 {
                     test: /\.scss$/,
                     loader: 'style!css!sass'
-                },{
+                }, {
                     test: /\.html$/,
                     exclude: /node_modules/,
                     loader: 'raw'
@@ -82,6 +63,16 @@ module.exports = function makeWebpackConfig(options) {
         plugins: []
     };
     if (!options.TEST && !options.BUILD) {
+        var browserSync = new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 8080,
+            server: {
+                baseDir: 'dev'
+            },
+            ui: false,
+            online: false,
+            notify: false
+        });
         config.devtool = 'source-map';
         config.plugins.push(browserSync);
         config.devServer = {
@@ -92,16 +83,38 @@ module.exports = function makeWebpackConfig(options) {
         config.plugins.push(new HtmlWebpackPlugin({
             template: './app/index.html',
             inject: 'body',
-            minify: options.BUILD
+            minify: options.BUILD,
+            hash: true
         }));
         config.plugins.push(new webpack.NoErrorsPlugin(),
             // Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
             // Minify all javascript, switch loaders to minimizing mode
-            new webpack.optimize.UglifyJsPlugin());
+            new webpack.optimize.UglifyJsPlugin({
+                output: {comments: false}
+            }),
+            new webpack.optimize.DedupePlugin(),
+            new webpack.optimize.OccurenceOrderPlugin(true),
+            new CompressionPlugin({
+                asset: "{file}.gz",
+                algorithm: "gzip",
+                regExp: /\.js$|\.html$/,
+                threshold: 10240,
+                minRatio: 0.8
+            }));
     }
     if (options.TEST) {
         config.context = __dirname + '/app';
         config.entry = './index.ts';
+        config.module.postLoaders = [
+            {
+                test: /\.ts$/,
+                exclude: [
+                    /node_modules/,
+                    /spec.ts$/
+                ],
+                loader: 'istanbul-instrumenter'
+            }
+        ]
     }
     config.plugins.push(new webpack.DefinePlugin({
         ON_TEST: process.env.NODE_ENV === 'test'
